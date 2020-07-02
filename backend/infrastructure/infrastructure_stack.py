8dcb8be6d1e3acba0e797dba7ccae5d9e46161ec
Anna Aitchison <anna11199810@hotmail.co.uk>
2020-07-02 16:21:32 +0100

    Add outline of API and Lambda

--- backend/infrastructure/infrastructure_stack.py
+++ backend/infrastructure/infrastructure_stack.py
@@ -10,6 +10,7 @@ class InfrastructureStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        # ******* Database
         rehomerTable = aws_dynamodb.Table(
             self, "rehomerTable",
             partition_key=aws_dynamodb.Attribute(
@@ -21,18 +22,40 @@ class InfrastructureStack(core.Stack):
             billing_mode=aws_dynamodb.BillingMode.PROVISIONED,
             time_to_live_attribute="expires"
         )
-        # ******* Lambda functions
-        '''rehomer_lambda_function = aws_lambda.Function(self, "HandleBookUploadLambda",
-                                                          handler='app.lambda_handler',
-                                                          runtime=aws_lambda.Runtime.PYTHON_3_8,
-                                                          code=aws_lambda.Code.from_asset(
-                                                              '../Functions/handlers/handle_book_upload'))'''
 
-        # ******* S3 upload buckets
+        # ******* Lambdas and API gateway
+        # Create simple, publically available API gateway resource. The CORS stuff is only for preflight requests
+        rescue_centre_api = aws_apigateway.RestApi(self, 'rescueCentreAPI', rest_api_name='rescueCentreAPI',
+                                                default_cors_preflight_options={
+                                                    "allow_origins": ["*"],
+                                                    "allow_methods": ["GET", "POST", "OPTIONS"]
+                                                })
+
+        # Create URL paths
+        rehomers_resource = rescue_centre_api.root.add_resource('rehomers')
+        rehomers_lambda_function = aws_lambda.Function(self, "rehomers_lambda",
+                                              handler='app.lambda_handler',
+                                              runtime=aws_lambda.Runtime.PYTHON_3_8,
+                                              code=aws_lambda.Code.from_asset(
+                                                  "../lambdas/rehomersLambda"),
+                                              )
+        # Make intergration
+        rehomers_lambda_integration = aws_apigateway.LambdaIntegration(rehomers_lambda_function, proxy=True)
+        rehomers_resource.add_method('POST', rehomers_lambda_integration)
+        # ******* environment variables
+        #rehomer_lambda_function.add_environment(
+        #    "TABLE_NAME", rehomerTable.table_name)
+
+        # ******* function permissions
+        #rehomerTable.grant_write_data(rehomer_lambda_function)
+
+        # ******* S3 bucket
         websiteBucket = aws_s3.Bucket(self, "websiteBucket",
            public_read_access=True,      
            website_index_document="index.html"
         )
+
+        # ******* CloudFront distribution
         distribution = aws_cloudfront.CloudFrontWebDistribution(self, "websiteBucketDistribution",
            origin_configs=[aws_cloudfront.SourceConfiguration(
                s3_origin_source=aws_cloudfront.S3OriginConfig(
@@ -41,16 +64,12 @@ class InfrastructureStack(core.Stack):
                behaviors=[aws_cloudfront.Behavior(is_default_behavior=True)]
            )
            ]
-       )
+        )
+
+        # ******* Deploy to bucket
         deployment = aws_s3_deployment.BucketDeployment(self, "deployStaticWebsite", 
            sources=[aws_s3_deployment.Source.asset("../frontend")],
            destination_bucket=websiteBucket,
            distribution=distribution
         )
 
-        # ******* environment variables
-        #rehomer_lambda_function.add_environment(
-        #    "TABLE_NAME", rehomerTable.table_name)
-
-        # ******* function permissions
-        #rehomerTable.grant_write_data(rehomer_lambda_function)
